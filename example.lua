cf0e3dbd84e94108f986de80f160a92530fc8e0a
sang <61368081+sangwriter@users.noreply.github.com>
2023-02-23 23:53:05 -0800

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,7 +1,7 @@
 local Link = "https://raw.githubusercontent.com/sangwriter/Roblox-Bot-Wrapper/main/Bot%20Wrapper%20Main.lua"
 local BotAPI = loadstring(game:HttpGet(Link))()
 
-local Poopity = BotAPI:Launch()
+local Poopity = BotAPI:Launch(game.PlaceId, game.JobId)
 
 Poopity:Chat("bot wrapper test")
 
