c468da233b0d30581c05966a098fe0f1c2d8d36b
sang <61368081+sangwriter@users.noreply.github.com>
2023-02-24 01:40:30 -0800

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,7 +1,7 @@
 local Link = "https://raw.githubusercontent.com/sangwriter/Roblox-Bot-Wrapper/main/Bot%20Wrapper%20Main.lua"
 local BotAPI = loadstring(game:HttpGet(Link))()
 
-local Poopity = BotAPI:Launch(game.PlaceId, game.JobId)
+local Poopity = BotAPI:Launch(game.PlaceId, game.JobId) -- Bots automatically replace themselves if theyre banned or kicked
 
 Poopity:Chat("bot wrapper test")
 
